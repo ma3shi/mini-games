fba751b61e61251b7e2cf2bbd2f6595f2a6551e5
Masashi Itabashi <masa.itabashi@gmail.com>
2021-11-14 14:37:24 +0900

    3ゲーム追加

--- number_memory/script.js
+++ number_memory/script.js
@@ -1,34 +1,34 @@
-'use strict';
+"use strict";
 
 // ルール
 class Rule {
   constructor() {
-    this.ruleContent = document.getElementById('rule'); //ルール内容
-    const ruleBtn = document.getElementById('show-rule'); //ルールボタン
-    this.overlayBlur = document.getElementById('overlay-blur'); //ぼかし
-    const closeRuleBtn = document.getElementById('close-rule'); //ルールを閉じるボタン
+    this.ruleContent = document.getElementById("rule"); //ルール内容
+    const ruleBtn = document.getElementById("show-rule"); //ルールボタン
+    this.overlayBlur = document.getElementById("overlay-blur"); //ぼかし
+    const closeRuleBtn = document.getElementById("close-rule"); //ルールを閉じるボタン
     // アローでthisをparentへ
-    ruleBtn.addEventListener('click', () => this.openRule()); //ルールを開く
-    closeRuleBtn.addEventListener('click', () => this.closeRule()); //ルールを閉じる
-    this.overlayBlur.addEventListener('click', () => this.closeoverlayBlur()); //ぼかしを消す
+    ruleBtn.addEventListener("click", () => this.openRule()); //ルールを開く
+    closeRuleBtn.addEventListener("click", () => this.closeRule()); //ルールを閉じる
+    this.overlayBlur.addEventListener("click", () => this.closeoverlayBlur()); //ぼかしを消す
   }
 
   //ルールを開く
   openRule() {
-    this.ruleContent.classList.add('show');
-    this.overlayBlur.classList.remove('hidden');
+    this.ruleContent.classList.add("show");
+    this.overlayBlur.classList.remove("hidden");
   }
 
   //ルールを閉じる
   closeRule() {
-    this.ruleContent.classList.remove('show');
-    this.overlayBlur.classList.add('hidden');
+    this.ruleContent.classList.remove("show");
+    this.overlayBlur.classList.add("hidden");
   }
 
   //ぼかしを消す
   closeoverlayBlur() {
-    this.ruleContent.classList.remove('show');
-    this.overlayBlur.classList.add('hidden');
+    this.ruleContent.classList.remove("show");
+    this.overlayBlur.classList.add("hidden");
   }
 }
 
@@ -36,7 +36,7 @@ class Rule {
 class Card {
   constructor(game) {
     this.game = game; //ゲーム
-    this.cardWrapper = document.getElementById('card-wrapper'); //カード全体
+    this.cardWrapper = document.getElementById("card-wrapper"); //カード全体
     this.nums = []; //数値配列
     this.cards = []; //カード配列
   }
@@ -47,29 +47,29 @@ class Card {
       this.nums.push(i);
     }
     this.game.randomNum = Math.floor(Math.random() * this.game.choices) + 1; //正解
-    console.log(this.game.randomNum); //正解
+    // console.log(this.game.randomNum); //正解
   }
 
   //カード作成
   makeCards() {
     for (let i = 1; i <= this.game.choices; i++) {
       //表面
-      const cardFront = document.createElement('div'); //div要素作成
-      cardFront.classList.add('card-front'); //class追加
+      const cardFront = document.createElement("div"); //div要素作成
+      cardFront.classList.add("card-front"); //class追加
       cardFront.textContent = this.nums.splice(
         Math.floor(Math.random() * this.nums.length),
         1
       )[0]; //数字をランダムに切り出す
-      console.log(cardFront.textContent); //カードの数字
+      // console.log(cardFront.textContent); //カードの数字
 
       //裏面
-      const cardBack = document.createElement('div'); //div要素作成
-      cardBack.classList.add('card-back'); //class追加
-      cardBack.textContent = '?'; //?を表示
+      const cardBack = document.createElement("div"); //div要素作成
+      cardBack.classList.add("card-back"); //class追加
+      cardBack.textContent = "?"; //?を表示
       //カード
-      const card = document.createElement('div'); //div要素作成
-      card.setAttribute('id', i); //id属性追加
-      card.classList.add('card'); //class追加
+      const card = document.createElement("div"); //div要素作成
+      card.setAttribute("id", i); //id属性追加
+      card.classList.add("card"); //class追加
       card.appendChild(cardFront); //表面を子要素に
       card.appendChild(cardBack); //裏面を子要素に
       this.cards.push(card); //配列に追加
@@ -89,53 +89,53 @@ class Game {
   constructor() {
     const rule = new Rule(); //ルール
     this.card = new Card(this); //カード
-    this.spinBtn = document.getElementById('spin-btn'); //spinボタン
-    this.quensionBtn = document.getElementById('question-btn'); //questionボタン
-    this.nextBtn = document.getElementById('next-btn'); //nextボタン
-    this.retryBtn = document.getElementById('retry-btn'); //retrytボタン
-    this.message = document.getElementById('message'); //メッセージ
+    this.spinBtn = document.getElementById("spin-btn"); //spinボタン
+    this.quensionBtn = document.getElementById("question-btn"); //questionボタン
+    this.nextBtn = document.getElementById("next-btn"); //nextボタン
+    this.retryBtn = document.getElementById("retry-btn"); //retrytボタン
+    this.message = document.getElementById("message"); //メッセージ
 
-    this.rollSound = document.getElementById('roll-sound'); //ロール音
-    this.questionSound = document.getElementById('question-sound'); //出題音
-    this.correctSound = document.getElementById('correct-sound'); //正解音
-    this.allCorrectSound = document.getElementById('allcorrect-sound'); //全問正解音
-    this.unCorrectSound = document.getElementById('uncorrect-sound'); //不正解音
+    this.rollSound = document.getElementById("roll-sound"); //ロール音
+    this.questionSound = document.getElementById("question-sound"); //出題音
+    this.correctSound = document.getElementById("correct-sound"); //正解音
+    this.allCorrectSound = document.getElementById("allcorrect-sound"); //全問正解音
+    this.unCorrectSound = document.getElementById("uncorrect-sound"); //不正解音
 
-    this.stageEl = document.getElementById('stage'); //ステージ表示
-    this.stage = 'undefind'; //ステージ
+    this.stageEl = document.getElementById("stage"); //ステージ表示
+    this.stage = "undefind"; //ステージ
     this.maxStage = 5; //最大ステージ
-    this.progressBarFull = document.getElementById('progress-bar-full'); //プログレスバー
-    this.cardCount = 'undefind'; //カード枚数　３から
+    this.progressBarFull = document.getElementById("progress-bar-full"); //プログレスバー
+    this.cardCount = "undefind"; //カード枚数　３から
     this.maxCardCount = 8; //最高枚数
-    this.choices = 'undefind'; //選択数
-    this.randomNum = 'undefind'; //正解
-    this.spinCount = 'undefind'; //カード回転数
+    this.choices = "undefind"; //選択数
+    this.randomNum = "undefind"; //正解
+    this.spinCount = "undefind"; //カード回転数
 
     //spinボタン
-    this.spinBtn.addEventListener('click', () => {
-      if (this.spinBtn.classList.contains('disabled')) return; //spinボタンが既に押してある
-      this.spinBtn.classList.add('disabled'); //spinボタンを押せなくする
+    this.spinBtn.addEventListener("click", () => {
+      if (this.spinBtn.classList.contains("disabled")) return; //spinボタンが既に押してある
+      this.spinBtn.classList.add("disabled"); //spinボタンを押せなくする
       this.rollSound.play(); //ロール音
       this.spinCard(); //カード回転
     });
 
     //questionボタン
-    this.quensionBtn.addEventListener('click', () => {
-      if (this.quensionBtn.classList.contains('disabled')) return; //questionボタンが既に押してある
-      this.quensionBtn.classList.add('disabled'); //questionボタンを押せなくする
+    this.quensionBtn.addEventListener("click", () => {
+      if (this.quensionBtn.classList.contains("disabled")) return; //questionボタンが既に押してある
+      this.quensionBtn.classList.add("disabled"); //questionボタンを押せなくする
       this.question(); //出題
     });
 
     //nextボタン
-    this.nextBtn.addEventListener('click', () => {
-      this.nextBtn.classList.add('disabled'); //nextボタンを押せなくする
+    this.nextBtn.addEventListener("click", () => {
+      this.nextBtn.classList.add("disabled"); //nextボタンを押せなくする
       this.stage++;
       this.resetContent(); //リセット
     });
 
     //retryボタン
-    this.retryBtn.addEventListener('click', () => {
-      this.retryBtn.classList.add('disabled'); //retryボタンを押せなくする
+    this.retryBtn.addEventListener("click", () => {
+      this.retryBtn.classList.add("disabled"); //retryボタンを押せなくする
       this.initGame(); //ゲーム初期化
     });
     this.initGame(); //ゲーム初期化
@@ -145,13 +145,13 @@ class Game {
   spinCard() {
     if (this.choices > 0) {
       setTimeout(() => {
-        this.card.cards[this.spinCount].classList.add('spin'); //spinクラスを追加
+        this.card.cards[this.spinCount].classList.add("spin"); //spinクラスを追加
         this.spinCount++; //カード回転数
         this.spinCard(); //カード回転
       }, 500);
       this.choices--; //choicesを減らしているので注意!!
     } else {
-      this.quensionBtn.classList.remove('disabled'); //questionボタンを押せるようにする
+      this.quensionBtn.classList.remove("disabled"); //questionボタンを押せるようにする
     }
   }
 
@@ -159,21 +159,21 @@ class Game {
   question() {
     this.rollSound.pause(); //ロール音ストップ
     this.questionSound.play(); //出題音
-    this.card.cardWrapper.classList.remove('inactive'); //カードをめくれるようにする
+    this.card.cardWrapper.classList.remove("inactive"); //カードをめくれるようにする
     const cards = Array.from(this.card.cardWrapper.children); //HTMLCollectionを配列に変換
-    cards.forEach(card => card.classList.add('cursor-change')); //カーソルをpointerへ
+    cards.forEach((card) => card.classList.add("cursor-change")); //カーソルをpointerへ
     this.message.textContent = `${this.randomNum} は？`; //出題表示
     this.selectAnswer(); //解答選択
   }
 
   ///解答選択
   selectAnswer() {
-    this.card.cards.forEach(card => {
-      card.addEventListener('click', () => {
-        if (this.card.cardWrapper.classList.contains('inactive')) return; //カードが既にめくれない状態
-        this.card.cardWrapper.classList.add('inactive'); //カード全体がめくれない状態にする
+    this.card.cards.forEach((card) => {
+      card.addEventListener("click", () => {
+        if (this.card.cardWrapper.classList.contains("inactive")) return; //カードが既にめくれない状態
+        this.card.cardWrapper.classList.add("inactive"); //カード全体がめくれない状態にする
 
-        card.classList.add('turnover'); //カードをめくる
+        card.classList.add("turnover"); //カードをめくる
         this.checkAnswer(card); //解答チェック
       });
     });
@@ -187,29 +187,29 @@ class Game {
       //全問正解
       if (this.cardCount === this.maxCardCount) {
         this.allCorrectSound.play(); //全問正解音
-        this.message.textContent = 'Game Clear !'; //メッセージ表示
-        this.retryBtn.classList.remove('disabled'); //もう一度ボタン使用可
+        this.message.textContent = "Game Clear !"; //メッセージ表示
+        this.retryBtn.classList.remove("disabled"); //もう一度ボタン使用可
         //全問正解以外
       } else {
         this.correctSound.play(); //正解音
-        this.message.textContent = '正解 !'; //メッセージ表示
-        this.nextBtn.classList.remove('disabled'); //次のレベルへボタン使用可
+        this.message.textContent = "正解 !"; //メッセージ表示
+        this.nextBtn.classList.remove("disabled"); //次のレベルへボタン使用可
       }
       //不正解
     } else {
       this.unCorrectSound.play(); //正解音
-      this.message.textContent = 'Game over'; //メッセージ表示
-      this.retryBtn.classList.remove('disabled'); //もう一度ボタン使用可
+      this.message.textContent = "Game over"; //メッセージ表示
+      this.retryBtn.classList.remove("disabled"); //もう一度ボタン使用可
     }
   }
 
   //リセット
   resetContent() {
-    this.spinBtn.classList.remove('disabled'); //spinボタン使用可
+    this.spinBtn.classList.remove("disabled"); //spinボタン使用可
     this.stageEl.textContent = `Stage ${this.stage}/${this.maxStage}`; //ステージ表示
     //プログレスバー
     this.progressBarFull.style.width = `${(this.stage / this.maxStage) * 100}%`;
-    this.message.textContent = ''; //メッセージ表示
+    this.message.textContent = ""; //メッセージ表示
     //カード表示で以前の子要素が存在する場合は削除
     while (this.card.cardWrapper.firstChild) {
       this.card.cardWrapper.removeChild(this.card.cardWrapper.firstChild);
--- tenpuzzle/script.js
+++ tenpuzzle/script.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 //undefindとnullの違い
 //他クラスのプロパティに直はok?
@@ -8,32 +8,32 @@
 // ルール
 class Rule {
   constructor() {
-    this.ruleContent = document.getElementById('rule'); //ルール内容
-    const ruleBtn = document.getElementById('show-rule'); //ルールボタン
-    this.overlayBlur = document.getElementById('overlay-blur'); //ぼかし
-    const closeRuleBtn = document.getElementById('close-rule'); //ルールを閉じるボタン
+    this.ruleContent = document.getElementById("rule"); //ルール内容
+    const ruleBtn = document.getElementById("show-rule"); //ルールボタン
+    this.overlayBlur = document.getElementById("overlay-blur"); //ぼかし
+    const closeRuleBtn = document.getElementById("close-rule"); //ルールを閉じるボタン
     // アローでthisをparentへ
-    ruleBtn.addEventListener('click', () => this.openRule()); //ルールを開く
-    closeRuleBtn.addEventListener('click', () => this.closeRule()); //ルールを閉じる
-    this.overlayBlur.addEventListener('click', () => this.closeoverlayBlur()); //ぼかしを消す
+    ruleBtn.addEventListener("click", () => this.openRule()); //ルールを開く
+    closeRuleBtn.addEventListener("click", () => this.closeRule()); //ルールを閉じる
+    this.overlayBlur.addEventListener("click", () => this.closeoverlayBlur()); //ぼかしを消す
   }
 
   //ルールを開く
   openRule() {
-    this.ruleContent.classList.add('show');
-    this.overlayBlur.classList.remove('hidden');
+    this.ruleContent.classList.add("show");
+    this.overlayBlur.classList.remove("hidden");
   }
 
   //ルールを閉じる
   closeRule() {
-    this.ruleContent.classList.remove('show');
-    this.overlayBlur.classList.add('hidden');
+    this.ruleContent.classList.remove("show");
+    this.overlayBlur.classList.add("hidden");
   }
 
   //ぼかしを消す
   closeoverlayBlur() {
-    this.ruleContent.classList.remove('show');
-    this.overlayBlur.classList.add('hidden');
+    this.ruleContent.classList.remove("show");
+    this.overlayBlur.classList.add("hidden");
   }
 }
 
@@ -41,10 +41,10 @@ class Rule {
 class Numbers {
   constructor(game) {
     this.game = game; //ゲーム
-    this.numbersEl = document.getElementById('numbers'); //4つの数字欄
-    this.firstNum = 'undefind'; //1番目に選んだ数字
-    this.secondNum = 'undefind'; //2番目に選んだ数字
-    this.leftCount = 'undefind'; //残り枚数
+    this.numbersEl = document.getElementById("numbers"); //4つの数字欄
+    this.firstNum = "undefind"; //1番目に選んだ数字
+    this.secondNum = "undefind"; //2番目に選んだ数字
+    this.leftCount = "undefind"; //残り枚数
   }
 
   //4つの数字をセット
@@ -60,12 +60,12 @@ class Numbers {
     }
 
     // 4枚の数字を1枚ずつli要素へ
-    question.forEach(num => {
-      const numEl = document.createElement('li'); //li要素作成
+    question.forEach((num) => {
+      const numEl = document.createElement("li"); //li要素作成
       numEl.textContent = num; //numを表示準備
-      numEl.classList.add('number'); //class追加
+      numEl.classList.add("number"); //class追加
       //クリックイベント
-      numEl.addEventListener('click', () => {
+      numEl.addEventListener("click", () => {
         this.clickNum(numEl);
       });
       this.numbersEl.appendChild(numEl); //4つの数字欄の子要素に追加
@@ -77,8 +77,8 @@ class Numbers {
     //使用不可か1番目に選んだ数字ならreturn
     //containで複数を選ぶとどちらも含んだという条件になるのでダメ
     if (
-      clickEl.classList.contains('disabled') ||
-      clickEl.classList.contains('firstselect')
+      clickEl.classList.contains("disabled") ||
+      clickEl.classList.contains("firstselect")
     ) {
       return;
       //1番目の数字がnullまたは計算記号が未選択の場合は1番目の数字
@@ -88,35 +88,35 @@ class Numbers {
       this.game.calcSymbols.culcSym === null
     ) {
       this.firstNum = clickEl.textContent; //clickElのtextcontentを1番目に選んだ数字として代入
-      console.log(`firstNum: ${this.firstNum}`);
+      // console.log(`firstNum: ${this.firstNum}`);
 
       this.removeFirstSelectClass(); //firstselectクラスを一旦全部削除
-      clickEl.classList.add('firstselect'); //clickElにfirstselectクラス追加
+      clickEl.classList.add("firstselect"); //clickElにfirstselectクラス追加
       //2番目の数字決定
     } else {
       this.secondNum = clickEl.textContent; //clickElのtextcontentを2番目に選んだ数字として代入
-      console.log(`secondNum: ${this.secondNum}`);
+      // console.log(`secondNum: ${this.secondNum}`);
       this.game.numsCalc(clickEl); //2つの数字を計算
     }
   }
 
   //firstselectクラスを一旦全部削除
   removeFirstSelectClass() {
-    const lists = document.querySelectorAll('.number');
-    lists.forEach(list => {
-      if (list.classList.contains('firstselect')) {
-        list.classList.remove('firstselect');
+    const lists = document.querySelectorAll(".number");
+    lists.forEach((list) => {
+      if (list.classList.contains("firstselect")) {
+        list.classList.remove("firstselect");
       }
     });
   }
 
   //2番目に選んだ数字を1番目に選んだ数字に変更
   secondToFirst(secondEl) {
-    let firstEl = document.querySelector('.firstselect'); //1番目に選んだliを取得
+    let firstEl = document.querySelector(".firstselect"); //1番目に選んだliを取得
     firstEl.textContent = null; //1番目に選んだliから数値を消す
-    firstEl.classList.add('disabled'); //1番目に選んだliにdisabledクラスをつける
-    firstEl.classList.remove('firstselect'); //1番目に選んだliからfirstselectクラスを削除
-    secondEl.classList.add('firstselect'); //2番目に選んだliにfirstselectクラスを追加
+    firstEl.classList.add("disabled"); //1番目に選んだliにdisabledクラスをつける
+    firstEl.classList.remove("firstselect"); //1番目に選んだliからfirstselectクラスを削除
+    secondEl.classList.add("firstselect"); //2番目に選んだliにfirstselectクラスを追加
     this.firstNum = this.secondNum; //２枚目の数値を1枚目の数値に代入
     this.secondNum = null; //２枚目の数値をnullに
   }
@@ -127,7 +127,7 @@ class Numbers {
     secondEl.textContent = this.secondNum; //計算結果を2番目に選んだliに表示
     this.secondToFirst(secondEl); //2番目に選んだ数字を1番目に選んだ数字に変更
     this.leftCount--; //numberの残りcountを1減らす
-    console.log(`leftCount: ${this.leftCount}`);
+    // console.log(`leftCount: ${this.leftCount}`);
   }
 }
 
@@ -135,21 +135,21 @@ class Numbers {
 class CalcSymbols {
   constructor(game) {
     this.game = game; //いらない？
-    this.calcSymsEl = document.getElementById('symbols'); //4つの記号欄
-    this.calcSyms = ['+', '-', '&times;', '&divide;']; //計算記号一覧
+    this.calcSymsEl = document.getElementById("symbols"); //4つの記号欄
+    this.calcSyms = ["+", "-", "&times;", "&divide;"]; //計算記号一覧
     // this.calcSyms = ['+', '-', '*', '/']; //計算記号一覧
-    this.culcSym = 'undefind'; //計算記号
+    this.culcSym = "undefind"; //計算記号
   }
   //計算記号をセット
   setCalcSyms() {
     this.culcSym = null; //計算記号を空に
     // 4枚の計算記号を1枚ずつli要素へ
-    this.calcSyms.forEach(sym => {
-      const calcSymEl = document.createElement('li'); //li要素作成
+    this.calcSyms.forEach((sym) => {
+      const calcSymEl = document.createElement("li"); //li要素作成
       calcSymEl.innerHTML = sym; //symを表示準備
-      calcSymEl.classList.add('symbol'); //class追加
+      calcSymEl.classList.add("symbol"); //class追加
       //クリックイベント
-      calcSymEl.addEventListener('click', () => {
+      calcSymEl.addEventListener("click", () => {
         this.clickCalcSym(calcSymEl);
       });
       this.calcSymsEl.appendChild(calcSymEl); //4つの記号欄の子要素に追加
@@ -159,25 +159,25 @@ class CalcSymbols {
   //計算記号をクリック
   clickCalcSym(clickEl) {
     //特殊文字で表示されている計算記号を元に戻す
-    if (clickEl.textContent === '×') {
-      this.culcSym = '*';
+    if (clickEl.textContent === "×") {
+      this.culcSym = "*";
       //特殊文字で表示されている計算記号を元に戻す
-    } else if (clickEl.textContent === '÷') {
-      this.culcSym = '/';
+    } else if (clickEl.textContent === "÷") {
+      this.culcSym = "/";
     } else {
       this.culcSym = clickEl.textContent; //計算記号を代入
     }
     this.removeActiveSym(); //計算記号からactiveクラスを一旦全部削除
-    clickEl.classList.add('active'); //clickElにactiveクラス追加
-    console.log(`culcSym: ${this.culcSym}`);
+    clickEl.classList.add("active"); //clickElにactiveクラス追加
+    // console.log(`culcSym: ${this.culcSym}`);
   }
 
   //計算記号からactiveクラスを一旦全部削除
   removeActiveSym() {
-    const lists = document.querySelectorAll('.symbol');
-    lists.forEach(list => {
-      if (list.classList.contains('active')) {
-        list.classList.remove('active');
+    const lists = document.querySelectorAll(".symbol");
+    lists.forEach((list) => {
+      if (list.classList.contains("active")) {
+        list.classList.remove("active");
       }
     });
   }
@@ -195,16 +195,16 @@ class Game {
     const rule = new Rule(); //ルール
     this.numbers = new Numbers(this); //数字
     this.calcSymbols = new CalcSymbols(this); //計算記号
-    this.stageEl = document.getElementById('stage'); //ステージ
-    this.progressBarFull = document.getElementById('progress-bar-full'); //プログレスバー
-    this.messageEl = document.getElementById('message'); //メッセージ
-    this.nextBtn = document.getElementById('next'); //nextボタン
-    this.retryBtn = document.getElementById('retry'); //retryボタン
-    this.allResetBtn = document.getElementById('allreset'); //all resetボタン
-    this.correctSound = document.getElementById('correct'); //正解音
-    this.allcorrectSound = document.getElementById('allcorrect'); //全問正解音
-    this.unCorrectSound = document.getElementById('uncorrect'); //不正解音
-    this.currentQsNum = 'undefind'; //現在の問題番号
+    this.stageEl = document.getElementById("stage"); //ステージ
+    this.progressBarFull = document.getElementById("progress-bar-full"); //プログレスバー
+    this.messageEl = document.getElementById("message"); //メッセージ
+    this.nextBtn = document.getElementById("next"); //nextボタン
+    this.retryBtn = document.getElementById("retry"); //retryボタン
+    this.allResetBtn = document.getElementById("allreset"); //all resetボタン
+    this.correctSound = document.getElementById("correct"); //正解音
+    this.allcorrectSound = document.getElementById("allcorrect"); //全問正解音
+    this.unCorrectSound = document.getElementById("uncorrect"); //不正解音
+    this.currentQsNum = "undefind"; //現在の問題番号
     //確認用問題
     // this.questions = [
     //   [1, 2, 3, 4],
@@ -232,19 +232,19 @@ class Game {
     ];
 
     //nextボタン
-    this.nextBtn.addEventListener('click', () => {
-      if (this.nextBtn.classList.contains('disabled')) return; //nextボタン利用不可ならreturn
+    this.nextBtn.addEventListener("click", () => {
+      if (this.nextBtn.classList.contains("disabled")) return; //nextボタン利用不可ならreturn
       this.resetGame(); //ゲームリセット
     });
 
     //retryボタン
-    this.retryBtn.addEventListener('click', () => {
-      if (this.retryBtn.classList.contains('disabled')) return; //nextボタン利用不可ならreturn
+    this.retryBtn.addEventListener("click", () => {
+      if (this.retryBtn.classList.contains("disabled")) return; //nextボタン利用不可ならreturn
       this.resetGame(); //ゲームリセット
     });
 
     //オールリセットボタン
-    this.allResetBtn.addEventListener('click', () => {
+    this.allResetBtn.addEventListener("click", () => {
       this.currentQsNum = 0; //現在の問題番号をリセット 配列を利用するので0にしておく
       this.resetGame(); //ゲームリセット
     });
@@ -258,7 +258,7 @@ class Game {
         this.numbers.firstNum +
         this.calcSymbols.culcSym +
         this.numbers.secondNum +
-        ')'
+        ")"
     )();
   }
   //選択した２つの数字を計算
@@ -269,7 +269,7 @@ class Game {
       this.calcSymbols.resetCalcSyms(); //計算記号をリセット
       return;
     }
-    console.log(`culcResultNum ${culcResultNum}`);
+    // console.log(`culcResultNum ${culcResultNum}`);
     this.numbers.numsResultProcess(secondEl, culcResultNum); //計算結果を表示
     this.calcSymbols.resetCalcSyms(); //計算記号をリセット
 
@@ -284,22 +284,22 @@ class Game {
 
       //全問正解
       if (this.currentQsNum === this.questions.length) {
-        this.messageEl.textContent = 'All Clear!!'; //メッセージ
-        this.messageEl.classList.remove('invisible'); //メッセージを出す
-        this.retryBtn.classList.add('disabled'); //retryボタン利用不可
+        this.messageEl.textContent = "All Clear!!"; //メッセージ
+        this.messageEl.classList.remove("invisible"); //メッセージを出す
+        this.retryBtn.classList.add("disabled"); //retryボタン利用不可
         this.allcorrectSound.play(); //全問正解音
         //全問正解以外
       } else {
-        this.messageEl.textContent = '成功!'; //メッセージ
-        this.messageEl.classList.remove('invisible'); //メッセージを出す
-        this.retryBtn.classList.add('disabled'); //retryボタン利用不可
-        this.nextBtn.classList.remove('disabled'); //nextボタン利用可
+        this.messageEl.textContent = "成功!"; //メッセージ
+        this.messageEl.classList.remove("invisible"); //メッセージを出す
+        this.retryBtn.classList.add("disabled"); //retryボタン利用不可
+        this.nextBtn.classList.remove("disabled"); //nextボタン利用可
         this.correctSound.play(); //正解音
       }
       //失敗
     } else {
-      this.messageEl.textContent = '失敗!'; //メッセージ
-      this.messageEl.classList.remove('invisible'); //メッセージを出す
+      this.messageEl.textContent = "失敗!"; //メッセージ
+      this.messageEl.classList.remove("invisible"); //メッセージを出す
       this.unCorrectSound.play(); //失敗音
     }
   }
@@ -315,9 +315,9 @@ class Game {
       ((this.currentQsNum + 1) / this.questions.length) * 100
     }%`;
 
-    this.messageEl.classList.add('invisible'); //メッセージを消す
-    this.retryBtn.classList.remove('disabled'); //retryボタン使用可
-    this.nextBtn.classList.add('disabled'); //nextボタン使用不可
+    this.messageEl.classList.add("invisible"); //メッセージを消す
+    this.retryBtn.classList.remove("disabled"); //retryボタン使用可
+    this.nextBtn.classList.add("disabled"); //nextボタン使用不可
   }
 
   //ゲームリセット
